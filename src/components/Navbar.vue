c461e49bbf95ce657e9c3817371c52df8962884d
SAK90 <sananthakrishnan_ee20b15_18@dtu.ac.in>
2023-08-08 17:56:07 +0530

    Added Navbar component and Tailwind config files

---
+++ src/components/Navbar.vue
@@ -0,0 +1,34 @@
+<template>
+    <Disclosure as="nav" class="bg-white-800" v-slot="{ open }">
+      <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+        <div class="relative flex h-16 items-center justify-between">
+          <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
+            <DisclosureButton class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+              <span class="absolute -inset-0.5" />
+              <span class="sr-only">Open main menu</span>
+              <Bars3Icon v-if="!open" class="block h-6 w-6" aria-hidden="true" />
+              <XMarkIcon v-else class="block h-6 w-6" aria-hidden="true" />
+            </DisclosureButton>
+          </div>
+          <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+            <div class="flex flex-shrink-0 items-center text-black font-medium text-2xl">
+              Create an invoice
+            </div>
+          </div>
+          <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+            <div class="flex space-x-4">
+                <button class='text-black hover:bg-gray-200 rounded-md px-3 py-2 text-sm font-medium border border-black border-solid'>Preview</button>
+                <button class='text-white bg-black rounded-md px-3 py-2 text-sm font-medium border border-black border-solid'>Download</button>
+            </div>
+          </div>
+        </div>
+      </div>
+  
+      
+    </Disclosure>
+  </template>
+  
+  <script setup>
+  import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
+  import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/vue/24/outline'
+  </script>
